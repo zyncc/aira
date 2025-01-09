938e111b62508d3460c6d7d603f715eb7eeb0430
zync <90741920+Zyncc1402@users.noreply.github.com>
2025-01-09 17:55:07 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPlaiceholder from "@plaiceholder/next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -12,4 +10,4 @@ const nextConfig = {
   },
 };
 
-withPlaiceholder(nextConfig);
+export default nextConfig;
