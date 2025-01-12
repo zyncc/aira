7a88c0957f439e39ecbc1627b91f4f3672c8a487
zync <90741920+Zyncc1402@users.noreply.github.com>
2025-01-12 22:53:36 +0530

    testing social login redirect

--- lib/authClient.ts
+++ lib/authClient.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const { useSession, signIn, signUp, signOut, admin } = createAuthClient({
-  baseURL: "https://pansy.in",
+  baseURL: "https://airaa.vercel.app",
   plugins: [adminClient()],
 });
