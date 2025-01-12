ccd33b1d8a6db072feffa5b5a78fc1fa7f2fff46
zync <90741920+Zyncc1402@users.noreply.github.com>
2025-01-12 21:24:35 +0530

    .

--- lib/authClient.ts
+++ lib/authClient.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const { useSession, signIn, signUp, signOut, admin } = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://pansy.in",
   plugins: [adminClient()],
 });
