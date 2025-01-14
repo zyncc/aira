2e6cd522f4a10385d177aeaa0b5053b3bb7c453b
zync <90741920+Zyncc1402@users.noreply.github.com>
2025-01-14 13:07:25 +0530

    new order activity

--- app/api/razorpayWebhook/route.ts
+++ app/api/razorpayWebhook/route.ts
@@ -1,13 +1,22 @@
+import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  const session = await auth.api.getSession({
+    headers: headers(),
+  });
+  if (!session?.session) {
+    return null;
+  }
+
   const rzp_response = await req.json();
   const paymentId = rzp_response.payload.payment.entity.id;
   const orderId = rzp_response.payload.payment.entity.order_id;
 
   // update order
-  await prisma.order.updateMany({
+  const order = await prisma.order.updateMany({
     where: {
       rzpOrderId: orderId,
     },
@@ -16,6 +25,19 @@ export async function POST(req: Request) {
       paymentSuccess: true,
     },
   });
+
+  for (let i = 0; i <= order.count; i++) {
+    const createActivity = async () => {
+      await prisma.activity.create({
+        data: {
+          userId: session.user.id,
+          title: "New Order Created",
+          type: "order",
+        },
+      });
+    };
+    createActivity();
+  }
 
   return NextResponse.json(rzp_response);
 }
