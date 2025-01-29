223e68d03204725cf18cb428796cd8aca1526fb6
zync <chandankrishna288@gmail.com>
2025-01-30 01:11:46 +0530

    test

--- public/sw.js
+++ public/sw.js
@@ -18,5 +18,5 @@ self.addEventListener('push', function (event) {
   self.addEventListener('notificationclick', function (event) {
     console.log('Notification click received.')
     event.notification.close()
-    event.waitUntil(clients.openWindow('<https://pansy.in>'))
+    event.waitUntil(clients.openWindow('https://pansy.in'))
   })
