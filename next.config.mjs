107641e113c815a1243b62650ab56677de98f45d
zync <chandankrishna288@gmail.com>
2025-06-21 17:22:10 +0530

    increased server action body size limit

--- next.config.mjs
+++ next.config.mjs
@@ -29,15 +29,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/_next/image",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
         source: "/(.*)",
         headers: [
           {
