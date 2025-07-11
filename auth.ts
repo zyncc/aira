3a2db1895194c1af4ffeb5967766a8a64c53fe13
zync <chandankrishna288@gmail.com>
2025-07-11 21:31:49 +0530

    test 3

--- auth.ts
+++ auth.ts
@@ -112,7 +112,7 @@ export const auth = betterAuth({
     },
     crossSubDomainCookies: {
       enabled: true,
-      domain: "admin.airaclothing.in",
+      domain: "airaclothing.in",
     },
   },
   advanced: {
