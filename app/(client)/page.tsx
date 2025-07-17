c86ae5dd59ce332efda6cca190f7d00a245fc759
Chandan <chandankrishna288@gmail.com>
2025-07-17 06:27:51 -0700

    ..

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -10,7 +10,7 @@ export default async function HomePage() {
       {/* <PreLoader /> */}
       {/* <HeroImages /> */}
       <OurCollection />
-      <Footer />
+      {/* <Footer /> */}
       {/* <Banner /> */}
       <CookieConsentBar />
     </main>
