533cbf7eaa84bc57fcbfbe5260052f2596bc4da9
Chandan <chandankrishna288@gmail.com>
2025-07-18 02:50:31 -0700

    ..

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/footer/footer";
-// import PreLoader from "@/components/preloader";
+import PreLoader from "@/components/preloader";
 import OurCollection from "@/components/carousel/ourCollection";
 import HeroImages from "@/components/heroImages";
 import CookieConsentBar from "@/components/CookieConsent";
@@ -7,8 +7,8 @@ import CookieConsentBar from "@/components/CookieConsent";
 export default async function HomePage() {
   return (
     <main className="flex-1">
-      {/* <PreLoader /> */}
-      {/* <HeroImages /> */}
+      <PreLoader />
+      <HeroImages />
       <OurCollection />
       <Footer />
       {/* <Banner /> */}
