47cceedce04b5b81b30c8dc502cbe1ddddddbf53
zyncc <chandankrishna288@gmail.com>
2025-08-21 19:28:33 +0530

    .

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,3 +1,9 @@
+import { Container } from "@/components/container";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Container className="flex h-screen w-full items-center justify-center">
+      <h1>TEST</h1>
+    </Container>
+  );
 }
