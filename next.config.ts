2f0284cf6ea7226cbfefecfc88c0a49eb5305440
zyncc <chandankrishna288@gmail.com>
2025-08-28 14:21:44 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  generateBuildId: async () => {
-    return process.env.GITHUB_SHA as string;
-  },
   experimental: {
     ppr: "incremental",
     typedEnv: true,
