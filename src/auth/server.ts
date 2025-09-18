38c41cdd55f3e178e92b86a02bc230179be265ac
zyncc <chandankrishna288@gmail.com>
2025-09-18 16:05:08 +0530

    .

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -59,10 +59,10 @@ export const auth = betterAuth({
     database: {
       generateId: () => uuid(),
     },
-    // crossSubDomainCookies: {
-    //   enabled: process.env.NODE_ENV !== "development",
-    //   domain: ".airaclothing.in",
-    // },
+    crossSubDomainCookies: {
+      enabled: process.env.NODE_ENV !== "development",
+      domain: ".airaclothing.in",
+    },
   },
   databaseHooks: {
     user: {
