bdfb0eb95477377419871e094d49c867b3964331
zyncc <chandankrishna288@gmail.com>
2025-10-15 17:32:33 +0530

    signin page canonical

--- src/app/(client)/signin/page.tsx
+++ src/app/(client)/signin/page.tsx
@@ -4,6 +4,9 @@ import SignInComponent from "./_components/SignInComponent";
 export const metadata: Metadata = {
   title: "Sign up or Login to your account - Aira Clothing",
   description: "Login to your account to access your order history.",
+  alternates: {
+    canonical: "/signin",
+  },
 };
 
 export default async function SignInPage(props: {
