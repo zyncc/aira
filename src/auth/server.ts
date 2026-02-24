4ebeaa0112aa08af3e81f2508e28368b6c005ae9
zyncc <chandankrishna288@gmail.com>
2026-02-24 11:26:57 +0530

    .

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -70,7 +70,7 @@ export const auth = betterAuth({
     user: {
       create: {
         after: async (user) => {
-          await sendWelcomeEmail(user.email, user.name);
+          sendWelcomeEmail(user.email, user.name);
         },
       },
     },
