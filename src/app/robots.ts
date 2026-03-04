b62c4059257ed515a9281a8172afe46189c827b8
zyncc <chandankrishna288@gmail.com>
2026-03-04 14:27:49 +0530

    feat: Update `robots.ts` to include specific rules for social media bots and universally disallow `meta.json`.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,21 +2,29 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      crawlDelay: 0,
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/admin/",
-        "/admin",
-        "/refunds",
-        "/account",
-        "/checkout",
-        "/search",
-        "/reviews/all",
-        "/signin",
-      ],
-    },
+    rules: [
+      {
+        userAgent: ["facebookexternalhit", "meta-externalads"],
+        allow: "/",
+        disallow: ["/*meta.json$"],
+      },
+      {
+        userAgent: "*",
+        crawlDelay: 0,
+        allow: "/",
+        disallow: [
+          "/admin/",
+          "/admin",
+          "/refunds",
+          "/account",
+          "/checkout",
+          "/search",
+          "/reviews/all",
+          "/signin",
+          "/*meta.json$",
+        ],
+      },
+    ],
     sitemap: "https://airaclothing.in/sitemap.xml",
   };
 }
